65430f934ddb0302052fd274dc60bed51a2f5d1e
benvjohn <benvjohn@gmail.com>
2023-08-28 13:43:14 -0500

    added readme file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer")
+const fs = require("fs")
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -10,12 +11,21 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+fs.writeFile("README2.md", data, function(err){
+    if(err){
+        throw err
+    }else{
+        console.log('file written')
+    }
+})
+}
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions).then(function(data){
 console.log(data.title)
+writeToFile('goodbye')
     })
 }
 
