8cffc188939a144e658e69d581b4690c10bb5170
benvjohn <benvjohn@gmail.com>
2023-08-28 13:24:54 -0500

    added inquirer prompt

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer")
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        name: 'title',
+        message: 'what is the title of the readme?',
+        type: 'input',
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(function(data){
+console.log(data.title)
+    })
+}
 
 // Function call to initialize app
 init();
