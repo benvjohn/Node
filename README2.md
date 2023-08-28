f46805c4ed39c136de8cc52742cae4a113464428
benvjohn <benvjohn@gmail.com>
2023-08-28 13:58:10 -0500

    generate markdown

--- README2.md
+++ README2.md
@@ -1,1 +1,2 @@
-goodbye
+# homework9
+
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer")
 const fs = require("fs")
+const generateMarkdown = require("./utils/generateMarkdown")
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -24,8 +25,9 @@ fs.writeFile("README2.md", data, function(err){
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions).then(function(data){
-console.log(data.title)
-writeToFile('goodbye')
+const string = generateMarkdown(data)
+console.log(string)
+writeToFile(string)
     })
 }
 
